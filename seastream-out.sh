4026f4282515a98edbd5c300adfbe99bd5010ccb
Luke Iannini <lukexi@me.com>
2020-12-05 22:03:46 +0000

    Split out stream def

--- seastream-out.sh
+++ seastream-out.sh
@@ -4,7 +4,10 @@ source /home/pi/seastream.config
 
 ICECAST_URL="icecast://source:$ICECAST_PASSWORD@seastream.live:1234/$LIVE_PATH"
 
+STREAMS="[f=mp3]$ICECAST_URL"
+# STREAMS+="|[f=rtp]rtp://$DIRECT_DESTINATION:1234"
+
 # Streams directly to one remote destination and also to seastream.live Icecast
 ffmpeg -f alsa -i plughw:1 -b:a $ENCODING_QUALITY -c:a libmp3lame \
     -content_type audio/mpeg -ice_name $STREAM_NAME -f tee -map 0 \
-    "[f=rtp]rtp://$DIRECT_DESTINATION:1234|[f=mp3]$ICECAST_URL"
+    $STREAMS
