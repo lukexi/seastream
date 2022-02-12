7150cc40e64867dba6f5e3096c66d8694172d56a
Luke Iannini <lukexi@me.com>
2022-02-12 00:31:16 -0500

    Use arecord for lower cpu usage vs ffmpeg alsa in

--- seastream-out.sh
+++ seastream-out.sh
@@ -6,7 +6,8 @@
 ENCODING_QUALITY=320k
 
 source /home/pi/seastream/config-icecast.private
-ICECAST_URL="icecast://source:$ICECAST_PASSWORD@seastream.live:1234/$ICECAST_PATH"
+ICECAST_SERVER=seastream.live:1234
+ICECAST_URL="icecast://source:$ICECAST_PASSWORD@$ICECAST_SERVER/$ICECAST_PATH"
 
 STREAMS="[f=mp3]$ICECAST_URL"
 
@@ -17,7 +18,12 @@ for DEST in $(cat config-destinations.private); do
     fi
 done
 
-# Streams directly to one remote destination and also to seastream.live Icecast
-ffmpeg -f alsa -i plughw:1 -b:a $ENCODING_QUALITY -c:a libmp3lame \
-    -content_type audio/mpeg -ice_name "$ICECAST_NAME" -ice_description "Seastream" -f tee -map 0 \
-    $STREAMS
+SOUNDCARD=hw:audioinjectorpi
+
+# Streams directly to one remote destination and also to seastream.live icecast
+# Uses workaround for high CPU usage bug in ffmpeg+alsa (-f alsa -i plughw:0) by getting audio via arecord instead
+arecord -q -N -M -t raw -D $SOUNDCARD -c 2 -r 48000 -f S16_LE | \
+    ffmpeg -f s16le -ac 2 -ar 48000 -i - \
+    -b:a $ENCODING_QUALITY -c:a libmp3lame \
+    -content_type audio/mpeg -ice_name "$ICECAST_NAME" \
+    -f tee -map 0 $STREAMS
