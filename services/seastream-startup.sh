bd4edff9a1d464bd503dd5feaa22ccc3bbfe9a86
Luke Iannini <lukexi@me.com>
2023-11-22 11:27:38 -0500

    fix audioinjector setup

--- services/seastream-startup.sh
+++ services/seastream-startup.sh
@@ -1,3 +1,3 @@
-amixer sset 'Line' cap
+amixer -D hw:CARD=audioinjectorpi sset 'Line' cap
 # amixer -D hw:CARD=audioinjectorpi set 'Output Mixer HiFi' unmute
 /home/pi/seastream/ssd1306/ssd1306_bin -n1 -I 128x64
