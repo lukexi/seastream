9bd92f7e085d5cba9a13b6a8dbe22f159b6dc066
Luke Iannini <lukexi@me.com>
2023-12-04 18:57:37 -0500

    upload check fix

--- services/seastream-upload.sh
+++ services/seastream-upload.sh
@@ -3,10 +3,11 @@ destination=proyekto.net:proyekto-server/files/seastream-archive
 echo "Uploading $source (if needed) to $destination..."
 rsync --progress --times $source $destination
 status=`rsync --times --dry-run --itemize-changes $source $destination`
+success=$?
 echo Status: $status
 if [[ $status == ">f"* ]]; then
     echo "File still needs upload"
-else
+elif [[ $success == 0 ]]; then
     echo "File is uploaded OK!"
     mv $source uploaded/
 fi
