08ab795a8a88dd337db48b5aa5a171b964768ea2
Luke Iannini <lukexi@me.com>
2024-01-05 16:04:33 -0500

    bugfixes

--- services/seastream-upload-recordings.sh
+++ services/seastream-upload-recordings.sh
@@ -1,5 +1,5 @@
 # run at 5 minutes after the hour
-for sample in `ls recordings/*.part`; do
+for sample in `find recordings -name *.part`; do
     # -Fa means show access type, 'aw' means accessed for writing
     is_being_written=`lsof -Fa -- $sample | tail -n 1`
     if [[ $is_being_written == "aw" ]]; then
@@ -9,7 +9,7 @@ for sample in `ls recordings/*.part`; do
     fi
 done
 
-for sample in `ls recordings/*.mp3`; do
+for sample in `find recordings -name *.mp3`; do
     bash services/seastream-upload.sh $sample
 done
 
--- services/seastream-upload.sh
+++ services/seastream-upload.sh
@@ -6,7 +6,7 @@ proyekto_destination=proyekto.net:proyekto-server/files/seastream-archive
 # Upload to proyekto if created between 4-6am
 if [[ $source == *_AM_04*.mp3 || $source == *_AM_05*.mp3 ]]; then
     echo "Uploading $source from 4-6am to $proyekto_destination"
-    rsync --remove-source-files --progress --times $source $proyekto_destination
+    rsync --progress --times $source $proyekto_destination
 fi
 
 # Upload all files to archive drive
